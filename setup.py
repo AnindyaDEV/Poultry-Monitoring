fdad26b86635df02648e58ecdfa7938033c488ae
Anindya Dey <105655927+AnindyaDEV@users.noreply.github.com>
2025-03-20 00:10:01 +0530

    notebook added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'PoultryDetection',
+    version= '0.0.0',
+    author= 'Anindya Dey',
+    author_email= 'anindya01wraith@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
